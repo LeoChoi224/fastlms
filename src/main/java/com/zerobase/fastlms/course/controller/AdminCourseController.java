72fd4eb28648cf0666831353bb1d6f63cb384ea7
HongmookChoi <leochoi224@gmail.com>
2025-07-17 15:01:54 +0900

    feat: feature/course-basic
    Implement course entity, repository, and simple course list & add feature

---
+++ src/main/java/com/zerobase/fastlms/course/controller/AdminCourseController.java
@@ -0,0 +1,60 @@
+package com.zerobase.fastlms.course.controller;
+
+import com.zerobase.fastlms.course.dto.CourseDto;
+import com.zerobase.fastlms.course.model.CourseInput;
+import com.zerobase.fastlms.course.model.CourseParam;
+import com.zerobase.fastlms.course.service.CourseService;
+import com.zerobase.fastlms.util.PageUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin/course")
+@Controller
+public class AdminCourseController extends BaseController{
+
+    private final CourseService courseService;
+
+    @GetMapping("/list.do")
+    public String list(Model model, CourseParam parameter) {
+
+        parameter.init();
+        List<CourseDto> courseList = courseService.list(parameter);
+
+        long totalCount = 0;
+        if (!CollectionUtils.isEmpty(courseList)) {
+            totalCount = courseList.get(0).getTotalCount();
+        }
+        String queryString = parameter.getQueryString();
+        String pagerHtml = getPaperHtml(totalCount, parameter.getPageSize(), parameter.getPageIndex(), queryString);
+
+        model.addAttribute("list", courseList);
+        model.addAttribute("totalCount", totalCount);
+        model.addAttribute("pageUtil", pagerHtml);
+
+        return "admin/course/list";
+    }
+
+    @GetMapping("/add.do")
+    public String add(Model model) {
+
+        return "admin/course/add";
+    }
+
+    @PostMapping("/add.do")
+    public String addSubmit(Model model, CourseInput parameter) {
+
+        boolean result = courseService.add(parameter);
+
+        return "redirect:/admin/course/list.do";
+    }
+
+
+}
---
+++ src/main/java/com/zerobase/fastlms/course/entity/Course.java
@@ -0,0 +1,38 @@
+package com.zerobase.fastlms.course.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String imagePath;
+    String keyword;
+    String subject;
+
+    @Column(length = 1000)
+    String summary;
+
+    @Lob
+    String contents;
+    long price;
+    long salePrice;
+    LocalDateTime saleEndDt;
+
+    LocalDateTime regDt; // 등록일(추가 날짜)
+    LocalDateTime udtDt; // 수정일(수정 날짜)
+
+}
